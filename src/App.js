f4498b40b95af7ecdd57b692c87cca0e54db6ea9
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-08-24 16:02:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import instaCartLogo from './logo.svg'
 import splash from './splash.webp'
 import styled from 'styled-components';
@@ -13,10 +12,21 @@ import Col from 'react-bootstrap/Col'
 const Splash = styled.div`
 width: 100vw;
 height: 100vh;
+background-repeat: no-repeat;
+background-size: auto;
 background-image: url(${splash})
 `
+const ContainerWithLargeLetters = styled(Container)`
+margin-top: 200px;
+font-size: 20px;
+`
+
 const SecContainer = styled(Container)`
 color: black;
+`
+
+const RowWithStores = styled(Row)`
+margin-top: 100px
 `
 
 function App() {
@@ -37,27 +47,27 @@ function App() {
      </Navbar.Collapse>
      </Navbar>
      <Splash>
-      <Container>
+      <ContainerWithLargeLetters>
         <Row>
           <Col><h1>Order groceries for delivery or pickup today</h1></Col>
         </Row>
-        <Row>
+        <RowWithStores>
           <Col>Store1</Col>
           <Col>Store2</Col>
           <Col>Store3</Col>
-        </Row>
+        </RowWithStores>
         <Row>
           <Col>
           <SecContainer>
-            <Row>
+            <RowWithStores>
             <Col>Store4</Col>
             <Col>Store5</Col>
             <Col>Store6</Col>
-            </Row>
+            </RowWithStores>
           </SecContainer>
           </Col>
         </Row>
-      </Container>
+      </ContainerWithLargeLetters>
      </Splash>
     </>
   );
